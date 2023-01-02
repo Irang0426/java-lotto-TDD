3fb2270b960ab5b4b85ec60b0a142a2ef4a08258
Irang <irang0426@gmail.com>
2023-01-02 15:37:15 +0900

    feat:보너스번호_숫자_검증

--- src/main/java/lotto/ValidationUtils.java
+++ src/main/java/lotto/ValidationUtils.java
@@ -1,10 +1,21 @@
 package lotto;
 
+import java.util.List;
+
 public class ValidationUtils {
     public static final int MIN_NO = 1;
     public static final int MAX_NO = 45;
 
     public static boolean validNo(int bonusNumber) {
         return bonusNumber >= MIN_NO && bonusNumber <= MAX_NO;
+    }
+
+    public static boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
--- src/test/java/lotto/ValidationUtilsTest.java
+++ src/test/java/lotto/ValidationUtilsTest.java
@@ -2,14 +2,23 @@ package lotto;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ValidationUtilsTest {
+
     @Test
-    void 보너스번호_1_45_검증() {
+    void 로또번호_1_45_검증() {
         assertThat(ValidationUtils.validNo(45)).isTrue();
         assertThat(ValidationUtils.validNo(1)).isTrue();
         assertThat(ValidationUtils.validNo(0)).isFalse();
         assertThat(ValidationUtils.validNo(46)).isFalse();
+    }
+
+    @Test
+    void 보너스번호_숫자_검증() {
+        assertThat(ValidationUtils.isInteger("1")).isTrue();
+        assertThat(ValidationUtils.isInteger("가나다")).isFalse();
     }
 }
